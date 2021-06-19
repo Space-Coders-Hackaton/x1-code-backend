a571fb536acfd7bb05441165fba7577db8d2324d
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2021-06-19 01:21:13 -0300

    feat: add redis setup

--- src/utils/started.ts
+++ src/utils/started.ts
@@ -3,7 +3,10 @@ export const started = (port: number, server_url: string): void => {
   console.log(`🔰  environment:\x1b[32m ${process.env.NODE_ENV} \x1b[0m`);
   console.log(`📢  node version:\x1b[32m ${process.versions.node} \x1b[0m`);
   if (process.env.NODE_ENV === 'development') console.log(`🌐  started on\x1b[34m ${server_url}:${port} \x1b[0m`);
-  if (process.env.SHOW_DOCS === 'true') console.log(`🌐  swagger\x1b[34m ${server_url}:${port}/api-docs \x1b[0m`);
+  if (process.env.SHOW_DOCS === 'true')
+    console.log(
+      `🌐  swagger\x1b[34m ${server_url}${process.env.NODE_ENV === 'development' ? `:${port}` : ''}/api-docs \x1b[0m`,
+    );
   if (process.env.DB_HOST && process.env.NODE_ENV === 'development') {
     console.log(`💾  database:\x1b[31m ${process.env.DB_NAME} \x1b[0m
            ╠═ host:\x1b[31m ${process.env.DB_COMPOSE_HOST || process.env.DB_HOST} \x1b[0m
